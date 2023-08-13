541474e3a04894a8a73d65ae100b97278b7d8b01
camewell <33259659+0xxcasper@users.noreply.github.com>
2023-08-13 08:03:36 +0700

    chore: remove require im age link

--- src/modules/Home/components/BookForm/BookForm.tsx
+++ src/modules/Home/components/BookForm/BookForm.tsx
@@ -34,9 +34,7 @@ const BookForm = () => {
       errors.description = 'Required.';
     }
 
-    if (!values.imageLink) {
-      errors.imageLink = 'Required.';
-    } else if (!values.imageLink.startsWith('https://')) {
+    if (values.imageLink && values.imageLink.startsWith('https://')) {
       errors.imageLink = 'Image link must be a valid url';
     }
 
@@ -174,6 +172,7 @@ const BookForm = () => {
               label="Link hình ảnh mẫu"
               onChange={handleChange}
               isTextArea={true}
+              isRequired={false}
               error={touched.imageLink && errors.imageLink}
             />
             <S.HeaderSection>Thông tin liên hệ</S.HeaderSection>
